348a2248bf12c4e242b91532d2e383b882c14877
Pawel Pabich <pawel@pabich.eu>
2017-06-09 17:32:32 +1000

    Upgraded to the latest contract

--- source/Octopus.Server.Extensibility.Authentication.Guest/GuestAuthenticationProvider.cs
+++ source/Octopus.Server.Extensibility.Authentication.Guest/GuestAuthenticationProvider.cs
@@ -24,8 +24,7 @@ namespace Octopus.Server.Extensibility.Authentication.Guest
             var authenticationProviderElement = new AuthenticationProviderElement
             {
                 Name = IdentityProviderName,
-                IsGuestProvider = true,
-                LinkHtml = "<div style=\"padding: 20px 20px 30px; text-align: center; \"><button class=\"btn btn-success\" type=\"button\" ng-click=\"signIn()\" focus-on=\"guest\" ng-disabled=\"isSubmitting.busy\">Sign in as a guest</button></div>"
+                IsGuestProvider = true
             };
 
             return authenticationProviderElement;
